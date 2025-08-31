fa74b75442a04a4e5cf4d440aaaf17ce26c0ad9b
66022848 <ken.24112547@gmail.com>
2025-08-31 16:28:27 +0700

    รันได้3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -101,10 +101,10 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DATABASE_NAME', 'attendance'),
-            'USER': os.getenv('DATABASE_USER', 'postgres'),
-            'PASSWORD': os.getenv('DATABASE_PASSWORD', 'postgres'),
-            'HOST': os.getenv('DATABASE_HOST', 'localhost'),
+            'NAME': os.getenv('DATABASE_NAME'),
+            'USER': os.getenv('DATABASE_USER'),
+            'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+            'HOST': os.getenv('DATABASE_HOST'),
             'PORT': os.getenv('DATABASE_PORT', '5432'),
         }
     }
