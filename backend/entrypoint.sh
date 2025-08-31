41c37ef04c6c1e30eb464488bb123ff4159ffc69
66022848 <ken.24112547@gmail.com>
2025-08-31 16:42:18 +0700

    รันได้4

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,6 +3,13 @@ set -e
 echo "Running migrations"
 python manage.py migrate --noinput
 
+python manage.py shell <<END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')
+END
+
 echo "Collecting static files"
 python manage.py collectstatic --noinput
 
